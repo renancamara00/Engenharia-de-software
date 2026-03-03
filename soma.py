56ce8329406f0429ed0e0a332b4e0f27d0b82be8
renancamara00 <renan.camara00@gmail.com>
2026-03-03 13:45:13 -0300

    testes

--- soma.py
+++ soma.py
@@ -1,5 +1,6 @@
 num1 = float (input("digite um numero: "))
 num2 = float (input("digite outro numero: "))
-soma = num1 + num2
-
-print(f"A soma é:{soma}")
+num3 = float (input("digite um terceiro numero:"))
+soma = num1 + num2 + num3
+2
+print(f"A soma é: {soma}")
