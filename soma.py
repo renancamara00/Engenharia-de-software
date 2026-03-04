d53946d7abbbf49d73f806a1ea9aa06a69d09bb0
Renan Câmara <renan.camara00@gmail.com>
2026-03-04 16:52:46 -0300

    Fix indentation and clean up soma.py

--- soma.py
+++ soma.py
@@ -2,5 +2,6 @@ num1 = float (input("digite um numero: "))
 num2 = float (input("digite outro numero: "))
 num3 = float (input("digite um terceiro numero:"))
 soma = num1 + num2 + num3
-2
+
 print(f"A soma é: {soma}")
+
